60677163351b91c6dd4ce003ea3f6ff8da941c2e
Mikssxed <112573426+Mikssxed@users.noreply.github.com>
2022-11-29 22:36:30 +0100

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+function start()
+{
+
+    var numbers: string = "";
+
+    for (let i = 1; i < 6; i++)
+    {
+        var element: string = "lit" + i; //creates id for every div with number
+        numbers = numbers + '<div onclick="check('+ i +')" class="numbers" id="'+ element +'">'+ i +'</div>'; //creates whole div with onclick, class, number and id
+    }
+
+    document.getElementById("rates").innerHTML = numbers; //changes innerHTML of div with id rates to created divs
+
+    
+}
+window.onload = start; //starts function
+
+function check(nr: number)
+{
+
+    for (let i = 1; i < 6; i++)
+    {
+        var element = "lit" + i; //creates id for every div with number
+        document.getElementById(element).style.background = "hsl(213, 19%, 18%)"; //changes div background
+    }
+    var element = "lit" + nr; //creates id
+    document.getElementById(element).style.background = "hsl(217, 12%, 63%)"; //changes div background
+
+    document.getElementById("selected").innerHTML = 'You selected '+ nr +' out of 5'; //changes innerHTML of selected rate
+
+    document.getElementById("button").setAttribute("onclick", "submit()"); //only if this function is activeted button can be clicked
+
+}
+
+function submit() //swaps divs
+{
+    document.getElementById("rating").style.display = "none";
+    document.getElementById("end").style.display = "flex";
+}
